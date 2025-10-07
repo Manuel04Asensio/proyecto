1aa06b7993780c0ab0e7ba4936634c39084a369e
Manuel <manuel.asensio125@gmail.com>
2025-10-07 20:43:46 +0200

    fotoCarrusel

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { ScrollView } from 'react-native'
+import { Image } from 'expo-image'
+
 
 export default function App() {
   const COLOR_FONDO="#121212"
@@ -9,12 +11,25 @@ export default function App() {
   return (
     <View style={[styles.contenedorPrincipal, {backgroundColor: COLOR_FONDO}]}>
       <ScrollView>
-        source={require('./assets/granada_light.jpg')}
-        style={{
-        width: '100%',
-        height: 250,
-        resizeMode: 'cover', // Escala manteniendo relación de aspecto
-      }}
+        <Image
+          source={require("./assets/granada_light.jpg")}
+          style={{
+            width: '100%',
+            height: 250,
+            resizeMode: 'cover',
+          }}
+        />
+      <View style={styles.contenedorSecundario}>
+          <Text style={[styles.titulo, {color: COLOR_TITULO}]}>
+            ¿Que hacer en Granada?</Text>
+          <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+          <Image source={require('./assets/actividad1.jpg')} style={styles.fotoCarrusel} />
+          <Image source={require('./assets/actividad2.jpg')} style={styles.fotoCarrusel} />
+          <Image source={require('./assets/actividad3.jpg')} style={styles.fotoCarrusel} />
+          <Image source={require('./assets/actividad4.jpg')} style={styles.fotoCarrusel} />
+          <Image source={require('./assets/actividad5.jpg')} style={styles.fotoCarrusel} />
+        </ScrollView>
+      </View>
       </ScrollView>
     </View>
   )
@@ -24,6 +39,24 @@ const styles = StyleSheet.create({
   contenedorPrincipal: {
     flex: 1,
     flexDirection: 'column',
+  },
+  contenedorSecundario: {
+    flex: 1,
+    marginHorizontal: 10,
+    padding: 5,
+    marginVertical: 20
+  },
+  titulo: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginVertical: 20,
+  },
+  fotoCarrusel: {
+    width: 250,
+    height: 300,
+    marginRight: 10,
+    borderRadius: 10,
+    alignSelf: 'center',
+    marginBottom: 15,
   }
-
 })
