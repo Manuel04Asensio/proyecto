4f2dcd1b3a35a80f5090150455ef4992788f29ff
Manuel <manuel.asensio125@gmail.com>
2025-10-14 21:32:34 +0200

    proyecto2

---
+++ components/Titulo.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Titulo() {
+  return (
+    <View>
+      <Text>Titulo</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
